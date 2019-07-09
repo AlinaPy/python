fed6fce192080664368b8ad8fcd03de38693c77d
ino4kayo <ino4kayo1@mail.ru>
2019-07-09 20:39:42 +0300

    Добавила нихера не умничку

--- file.py
+++ file.py
@@ -1,2 +1,7 @@
 print('Hello world!')
 print('Hi Marina!')
+answer = input("А ты в курсе, что можно запускать питоновский файл напрямую и вводить туда значения? (Y/N)")
+if answer3 == 'Y':
+    print("умничка")
+else:
+    print("нихера не умничка")
